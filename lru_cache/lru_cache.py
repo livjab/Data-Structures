259c9915b1c92df23c03a915cd6f7fa12384736f
Liv Johnson <livjab@gmail.com>
2019-10-16 00:26:07 -0500

    still working through some things

--- lru_cache/lru_cache.py
+++ lru_cache/lru_cache.py
@@ -1,3 +1,5 @@
+from doubly_linked_list import DoublyLinkedList
+
 class LRUCache:
     """
     Our LRUCache class keeps track of the max number of nodes it
@@ -7,7 +9,10 @@ class LRUCache:
     to every node stored in the cache.
     """
     def __init__(self, limit=10):
-        pass
+        self.limit = limit
+        self.current_size = 0
+        self.storage = DoublyLinkedList()
+        self.dict = {}
 
     """
     Retrieves the value associated with the given key. Also
@@ -17,7 +22,12 @@ class LRUCache:
     key-value pair doesn't exist in the cache.
     """
     def get(self, key):
-        pass
+        if key in self.dict:
+            node = self.dict[key]
+            self.storage.move_to_front(node)
+            return node
+        else:
+            return None
 
     """
     Adds the given key-value pair to the cache. The newly-
@@ -30,4 +40,11 @@ class LRUCache:
     the newly-specified value.
     """
     def set(self, key, value):
-        pass
+        if key in self.dict:
+            self.storage.delete(self.dict[key])
+            self.dict[key] = self.storage.add_to_head(value)
+            return
+
+        if self.current_size == self.limit:
+            old_key = self.storage.remove_from_tail()
+            self.current_size -= 1
